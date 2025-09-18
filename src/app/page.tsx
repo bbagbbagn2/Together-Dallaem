a3182a210c29fab0807182d03a9e45dfab622266
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-09-18 13:59:55 +0900

    Style : 컬러 팔레트 추가 및 기본 폰트 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="flex h-screen items-center justify-center text-2xl font-bold underline">
+    <h1 className="flex h-screen items-center justify-center text-xs font-light underline">
       TEAM5 화이팅 !!
     </h1>
   );
